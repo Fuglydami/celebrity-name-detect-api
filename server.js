c11318c59a6e9a47c71c4ddd4de6e3fe7861f5e8
Damilare Oyedeji <fuglydami@gmail.com>
2020-12-22 12:14:44 +0100

    adding port

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.post('/imageurl', (req, res) => {
   image.handleApiCall(req, res)
 })
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`)
 })
